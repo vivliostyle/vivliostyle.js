00dd8fec4ad16501cac4a5efc70a52bd6c4700ed
kwkbtr <kwkbtr@vivliostyle.com>
2015-04-14 18:32:41 +0900

    Remove source files

--- src/BUILD.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-java -jar ../../tools/closure/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --warning_level VERBOSE closure/goog/base.js adapt/*.js
-
-#java -jar ../../tools/closure/compiler.jar --compilation_level WHITESPACE_ONLY --formatting PRETTY_PRINT --warning_level VERBOSE closure/goog/base.js adapt/base.js adapt/task.js adapt/taskutil.js adapt/net.js adapt/cfi.js adapt/expr.js adapt/css.js adapt/csstok.js adapt/cssparse.js adapt/cssvalid.js adapt/geom.js adapt/cssprop.js adapt/xmldoc.js adapt/font.js adapt/sha1.js adapt/csscasc.js adapt/vtree.js adapt/cssstyler.js adapt/vgen.js adapt/pm.js adapt/layout.js adapt/ops.js adapt/toc.js adapt/epub.js adapt/devel.js adapt/vivliostyle-viewer.js
