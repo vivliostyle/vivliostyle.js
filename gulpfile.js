c5b15b13a25237dc027fa5559c7df3c1e368d825
kwkbtr <kwkbtr@vivliostyle.com>
2015-07-22 18:42:39 +0900

    Reorganize directory structure - All source files are placed under `src` directory - Files under `src/{fonts,html,scss}` are watched and built by gulp - Built files are placed under `build` directory, which is added to `.gitignore`.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,53 @@
 "use strict";
 
 var gulp = require("gulp");
+var changed = require("gulp-changed");
 var compass = require("gulp-compass");
 var path = require("path");
 var plumber = require("gulp-plumber");
 var notify = require("gulp-notify");
 
-var SCSS_FILES = "res-dev/scss/*.scss";
+var SRC_FILES = {
+    html: "src/html/*",
+    fonts: "src/fonts/*",
+    scss: "src/scss/*.scss"
+};
 
-gulp.task("default", ["watch"]);
+function copyTask(name, dest) {
+    dest = "build/" + dest;
+    return gulp.task("build:" + name, function() {
+        gulp.src(SRC_FILES[name])
+            .pipe(changed(dest))
+            .pipe(gulp.dest(dest));
+    });
+}
 
-gulp.task("watch", function() {
-    gulp.watch(SCSS_FILES, ["build:css"]);
-});
+// HTML files are copied to the root
+copyTask("html", "");
+copyTask("fonts", "fonts");
 
 gulp.task("build:css", function() {
-    gulp.src(SCSS_FILES)
+    gulp.src(SRC_FILES["scss"])
         .pipe(plumber({
             errorHandler: notify.onError("Error: <%= error.message %>")
         }))
         .pipe(compass({
-            config_file: "res-dev/config.rb",
-            css: path.resolve("res/css"),
-            sass: path.resolve("res-dev/scss")
+            config_file: "src/config.rb",
+            css: path.resolve("build/css"),
+            sass: path.resolve("src/scss")
         }));
 });
+
+gulp.task("build", [
+    "build:html",
+    "build:fonts",
+    "build:css"
+]);
+
+gulp.task("watch", ["build"], function() {
+    gulp.watch(SRC_FILES["html"], ["build:html"]);
+    gulp.watch(SRC_FILES["fonts"], ["build:fonts"]);
+    gulp.watch(SRC_FILES["scss"], ["build:css"]);
+});
+
+gulp.task("default", ["watch"]);
--- src/config.rb
+++ src/config.rb
@@ -1,7 +1,7 @@
 relative_assets = true
 project_path = "../"
-sass_dir = "res-dev/scss"
-css_dir = "res/css"
-fonts_dir = "res/fonts"
+sass_dir = "src/scss"
+css_dir = "build/css"
+fonts_dir = "build/fonts"
 output_style = ( environment == :production ) ? :compressed : :expanded
 line_comments = ( environment == :production ) ? false : true
