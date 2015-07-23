3b54c7d225d92b38e010f5dd419a8ff1e68e5279
kwkbtr <kwkbtr@vivliostyle.com>
2015-07-23 17:37:41 +0900

    Build Vivliostyle.js and copy files under `build` directory during `npm install`

---
+++ scripts/copy-vivliostyle-files.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var fs = require("fs-extra");
+var path = require("path");
+
+var BUILD_DIR = "build";
+var RES_DIR = "resources";
+var SRC_DIR = path.join("node_modules", "vivliostyle");
+var JS_LIB_DIR = path.join(BUILD_DIR, "js", "lib");
+var JS_FILENAME = "vivliostyle-viewer.min.js";
+
+fs.ensureDirSync(JS_LIB_DIR);
+fs.copySync(path.join(SRC_DIR, BUILD_DIR, JS_FILENAME), path.join(JS_LIB_DIR, JS_FILENAME));
+fs.copySync(path.join(SRC_DIR, RES_DIR), path.join(BUILD_DIR, RES_DIR));
