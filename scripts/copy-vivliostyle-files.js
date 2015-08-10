aa889a67f56d019926a49ecef5f0eac21b755ce3
kwkbtr <kwkbtr@vivliostyle.com>
2015-08-11 00:41:23 +0900

    Rename compiled Vivliostyle.js file

--- scripts/copy-vivliostyle-files.js
+++ scripts/copy-vivliostyle-files.js
@@ -7,7 +7,7 @@ var BUILD_DIR = "build";
 var RES_DIR = "resources";
 var SRC_DIR = path.join("node_modules", "vivliostyle");
 var JS_LIB_DIR = path.join(BUILD_DIR, "js", "lib");
-var JS_FILENAME = "vivliostyle-viewer.min.js";
+var JS_FILENAME = "vivliostyle.min.js";
 
 fs.ensureDirSync(JS_LIB_DIR);
 fs.copySync(path.join(SRC_DIR, BUILD_DIR, JS_FILENAME), path.join(JS_LIB_DIR, JS_FILENAME));
