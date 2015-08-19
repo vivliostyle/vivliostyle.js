a4b870e3f0705040073b632a6c998661b7c4be42
kwkbtr <kwkbtr@vivliostyle.com>
2015-08-19 11:26:20 +0900

    Expose constants under vivliostyle.constants

--- src/vivliostyle/constants.js
+++ src/vivliostyle/constants.js
@@ -2,22 +2,34 @@
  * Copyright 2015 Vivliostyle Inc.
  * @fileoverview Constants
  */
-goog.provide('vivliostyle.constants');
+goog.provide("vivliostyle.constants");
 
-/**
- * Page progression direction.
- * @enum {string}
- */
-vivliostyle.constants.PageProgression = {
-    LTR: "ltr",
-    RTL: "rtl"
-};
+goog.require("vivliostyle.namespace");
 
-/**
- * Page side (left/right).
- * @enum {string}
- */
-vivliostyle.constants.PageSide = {
-    LEFT: "left",
-    RIGHT: "right"
-};
+goog.scope(function() {
+    /**
+     * Page progression direction.
+     * @enum {string}
+     */
+    vivliostyle.constants.PageProgression = {
+        LTR: "ltr",
+        RTL: "rtl"
+    };
+    var PageProgression = vivliostyle.constants.PageProgression;
+    vivliostyle.namespace.exportSymbol("vivliostyle.constants.PageProgression", PageProgression);
+    goog.exportProperty(PageProgression, "LTR", PageProgression.LTR);
+    goog.exportProperty(PageProgression, "RTL", PageProgression.RTL);
+
+    /**
+     * Page side (left/right).
+     * @enum {string}
+     */
+    vivliostyle.constants.PageSide = {
+        LEFT: "left",
+        RIGHT: "right"
+    };
+    var PageSide = vivliostyle.constants.PageSide;
+    vivliostyle.namespace.exportSymbol("vivliostyle.constants.PageSide", PageSide);
+    goog.exportProperty(PageSide, "LEFT", PageSide.LEFT);
+    goog.exportProperty(PageSide, "RIGHT", PageSide.RTL);
+});
