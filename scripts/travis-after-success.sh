9a747f269a9f9dde723770c580b7322aa5978893
KAWAKUBO Toru <kwkbtr@vivliostyle.com>
2015-09-14 18:50:01 +0900

    Fix typo

--- scripts/travis-after-success.sh
+++ scripts/travis-after-success.sh
@@ -4,7 +4,7 @@ set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then
     version=$(grep '^ *"version":' package.json | sed -e 's/^.*"\([^"]*\)",$/\1/')
     if [ $(echo ${version} | grep -- '-pre$') ]; then
-        scripts/make-dist-packages.sh ${version}.$(date -u "+%Y%m%d%H%M%S")
+        scripts/make-dist-package.sh ${version}.$(date -u "+%Y%m%d%H%M%S")
     fi
 
     scripts/deploy-viewer-to-gh-pages.sh
