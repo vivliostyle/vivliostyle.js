c74bb0af1362d14a6064c7dcf751c9558dd6cd44
KAWAKUBO Toru <kwkbtr@vivliostyle.com>
2015-09-14 12:10:59 +0900

    Modify script deploying to gh-pages from Travis CI

--- scripts/travis-after-success.sh
+++ scripts/travis-after-success.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 set -ev
 
-# setup ssh key
-echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config
-echo -e "$GITHUB_DEPLOY_KEY" | base64 -d > ~/.ssh/deploy.key
-chmod 600 ~/.ssh/deploy.key
-
-cd ../
-
-# fetch gh-pages branch
-git clone --depth=1 --branch=gh-pages git@github.com:vivliostyle/vivliostyle.js.git gh-pages
-cd gh-pages
-
-# git configuration
-git config user.email "kwkbtr@vivliostyle.com"
-git config user.name "kwkbtr (Travis CI)"
-
-# update gh-pages branch
-cp -R ../vivliostyle-js-viewer/build/* viewer/
-git add viewer
-git commit -m "Update vivliostyle-js-viewer (original commit: $TRAVIS_COMMIT)"
-git push origin gh-pages
+if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then
+    build/deploy-viewer-to-gh-pages.sh
+fi
