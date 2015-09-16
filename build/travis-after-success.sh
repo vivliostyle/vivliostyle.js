02972df443b2f6b2169e1a3696f2c9fcbd96dbbf
KAWAKUBO Toru <kwkbtr@vivliostyle.com>
2015-09-16 16:31:28 +0900

    Publish to npm on tags

--- build/travis-after-success.sh
+++ build/travis-after-success.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" -a \( "${TRAVIS_BRANCH}" = "master" -o "${TRAVIS_TAG}" != "" \) ]; then
     build/npm-publish.sh
     build/deploy-viewer-to-gh-pages.sh
 fi
