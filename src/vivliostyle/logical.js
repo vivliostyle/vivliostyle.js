a680ec1b3eb30aa0995cb35b434afb0c6caac05a
KAWAKUBO Toru <kwkbtr@vivliostyle.com>
2015-10-12 20:27:36 +0900

    Consider `direction` property when converting physical and logical properties

--- src/vivliostyle/logical.js
+++ src/vivliostyle/logical.js
@@ -5,25 +5,50 @@
 goog.provide("vivliostyle.logical");
 
 goog.scope(function() {
+    /**
+     * @typedef {{regexp: !RegExp, to: string}}
+     */
+    var ConversionMap;
+
+    /**
+     * @param {!Object.<string, !Object.<string, !Array.<!{logical: string, physical: string}>>>} valueMaps
+     * @param {boolean} toPhysical
+     * @returns {!Object.<string, !Object.<string, !Array.<!ConversionMap>>>}
+     */
     function createRegExpMap(valueMaps, toPhysical) {
         var map = {};
-        Object.keys(valueMaps).forEach(function(writingMode) {
-            map[writingMode] = valueMaps[writingMode].map(function(p) {
-                var from = toPhysical ? p.logical : p.physical;
-                var to = toPhysical ? p.physical : p.logical;
-                return {
-                    regexp: new RegExp("(-?)" + from + "(-?)"),
-                    to: "$1" + to + "$2"
-                };
+        Object.keys(/** @type {!Object} */ (valueMaps)).forEach(function(writingMode) {
+            var dest = map[writingMode] = {};
+            var src = valueMaps[writingMode];
+            Object.keys(/** @type {!Object} */ (src)).forEach(function(direction) {
+                dest[direction] = src[direction].map(function(p) {
+                    var from = toPhysical ? p.logical : p.physical;
+                    var to = toPhysical ? p.physical : p.logical;
+                    return {
+                        regexp: new RegExp("(-?)" + from + "(-?)"),
+                        to: "$1" + to + "$2"
+                    };
+                });
             });
         });
         return map;
     }
 
-    function convert(value, writingMode, maps) {
-        var map = maps[writingMode];
-        if (!map) {
+    /**
+     * @param {string} value
+     * @param {string} writingMode
+     * @param {?string} direction
+     * @param {!Object.<string, !Object.<string, !Array.<!ConversionMap>>>} maps
+     * @returns {string}
+     */
+    function convert(value, writingMode, direction, maps) {
+        var maps2 = maps[writingMode];
+        if (!maps2) {
             throw new Error("unknown writing-mode: " + writingMode);
+        }
+        var map = maps2[direction || "ltr"];
+        if (!map) {
+            throw new Error("unknown direction: " + direction);
         }
         for (var i = 0; i < map.length; i++) {
             var p = map[i];
@@ -35,35 +60,73 @@ goog.scope(function() {
         return value;
     }
 
-    // TODO consider 'direction' property
+    /**
+     * @type {!Object.<string, !Object.<string, !Array.<!{logical: string, physical: string}>>>}
+     */
     var values = {
-        "horizontal-tb": [
-            {logical: "inline-start", physical: "left"},
-            {logical: "inline-end", physical: "right"},
-            {logical: "block-start", physical: "top"},
-            {logical: "block-end", physical: "bottom"}
-        ],
-        "vertical-rl": [
-            {logical: "inline-start", physical: "top"},
-            {logical: "inline-end", physical: "bottom"},
-            {logical: "block-start", physical: "right"},
-            {logical: "block-end", physical: "left"}
-        ],
-        "vertical-lr": [
-            {logical: "inline-start", physical: "top"},
-            {logical: "inline-end", physical: "bottom"},
-            {logical: "block-start", physical: "left"},
-            {logical: "block-end", physical: "right"}
-        ]
+        "horizontal-tb": {
+            "ltr": [
+                {logical: "inline-start", physical: "left"},
+                {logical: "inline-end", physical: "right"},
+                {logical: "block-start", physical: "top"},
+                {logical: "block-end", physical: "bottom"}
+            ],
+            "rtl": [
+                {logical: "inline-start", physical: "right"},
+                {logical: "inline-end", physical: "left"},
+                {logical: "block-start", physical: "top"},
+                {logical: "block-end", physical: "bottom"}
+            ]
+        },
+        "vertical-rl": {
+            "ltr": [
+                {logical: "inline-start", physical: "top"},
+                {logical: "inline-end", physical: "bottom"},
+                {logical: "block-start", physical: "right"},
+                {logical: "block-end", physical: "left"}
+            ],
+            "rtl": [
+                {logical: "inline-start", physical: "bottom"},
+                {logical: "inline-end", physical: "top"},
+                {logical: "block-start", physical: "right"},
+                {logical: "block-end", physical: "left"}
+            ]
+        },
+        "vertical-lr": {
+            "ltr": [
+                {logical: "inline-start", physical: "top"},
+                {logical: "inline-end", physical: "bottom"},
+                {logical: "block-start", physical: "left"},
+                {logical: "block-end", physical: "right"}
+            ],
+            "rtl": [
+                {logical: "inline-start", physical: "bottom"},
+                {logical: "inline-end", physical: "top"},
+                {logical: "block-start", physical: "left"},
+                {logical: "block-end", physical: "right"}
+            ]
+        }
     };
 
     var toPhysicalMaps = createRegExpMap(values, true);
-    vivliostyle.logical.toPhysical = function(value, writingMode) {
-        return convert(value, writingMode, toPhysicalMaps);
+    /**
+     * @param {string} value
+     * @param {string} writingMode
+     * @param {?string=} direction
+     * @returns {string}
+     */
+    vivliostyle.logical.toPhysical = function(value, writingMode, direction) {
+        return convert(value, writingMode, direction || null, toPhysicalMaps);
     };
 
     var toLogicalMaps = createRegExpMap(values, false);
-    vivliostyle.logical.toLogical = function(value, writingMode) {
-        return convert(value, writingMode, toLogicalMaps);
+    /**
+     * @param {string} value
+     * @param {string} writingMode
+     * @param {?string=} direction
+     * @returns {string}
+     */
+    vivliostyle.logical.toLogical = function(value, writingMode, direction) {
+        return convert(value, writingMode, direction || null, toLogicalMaps);
     };
 });
--- test/spec/vivliostyle/logical-spec.js
+++ test/spec/vivliostyle/logical-spec.js
@@ -4,7 +4,7 @@ describe("logical", function() {
     var l = vivliostyle.logical;
 
     describe("toPhysical", function() {
-        it("converts logical properties to physical properties (horizontal-tb)", function() {
+        it("converts logical properties to physical properties (horizontal-tb, ltr)", function() {
             expect(l.toPhysical("inline-start", "horizontal-tb")).toBe("left");
             expect(l.toPhysical("inline-end", "horizontal-tb")).toBe("right");
             expect(l.toPhysical("block-start", "horizontal-tb")).toBe("top");
@@ -15,7 +15,18 @@ describe("logical", function() {
             expect(l.toPhysical("border-block-end-width", "horizontal-tb")).toBe("border-bottom-width");
         });
 
-        it("converts logical properties to physical properties (vertical-rl)", function() {
+        it("converts logical properties to physical properties (horizontal-tb, rtl)", function() {
+            expect(l.toPhysical("inline-start", "horizontal-tb", "rtl")).toBe("right");
+            expect(l.toPhysical("inline-end", "horizontal-tb", "rtl")).toBe("left");
+            expect(l.toPhysical("block-start", "horizontal-tb", "rtl")).toBe("top");
+            expect(l.toPhysical("block-end", "horizontal-tb", "rtl")).toBe("bottom");
+            expect(l.toPhysical("border-inline-start-width", "horizontal-tb", "rtl")).toBe("border-right-width");
+            expect(l.toPhysical("border-inline-end-width", "horizontal-tb", "rtl")).toBe("border-left-width");
+            expect(l.toPhysical("border-block-start-width", "horizontal-tb", "rtl")).toBe("border-top-width");
+            expect(l.toPhysical("border-block-end-width", "horizontal-tb", "rtl")).toBe("border-bottom-width");
+        });
+
+        it("converts logical properties to physical properties (vertical-rl, ltr)", function() {
             expect(l.toPhysical("inline-start", "vertical-rl")).toBe("top");
             expect(l.toPhysical("inline-end", "vertical-rl")).toBe("bottom");
             expect(l.toPhysical("block-start", "vertical-rl")).toBe("right");
@@ -26,7 +37,18 @@ describe("logical", function() {
             expect(l.toPhysical("border-block-end-width", "vertical-rl")).toBe("border-left-width");
         });
 
-        it("converts logical properties to physical properties (vertical-lr)", function() {
+        it("converts logical properties to physical properties (vertical-rl, rtl)", function() {
+            expect(l.toPhysical("inline-start", "vertical-rl", "rtl")).toBe("bottom");
+            expect(l.toPhysical("inline-end", "vertical-rl", "rtl")).toBe("top");
+            expect(l.toPhysical("block-start", "vertical-rl", "rtl")).toBe("right");
+            expect(l.toPhysical("block-end", "vertical-rl", "rtl")).toBe("left");
+            expect(l.toPhysical("border-inline-start-width", "vertical-rl", "rtl")).toBe("border-bottom-width");
+            expect(l.toPhysical("border-inline-end-width", "vertical-rl", "rtl")).toBe("border-top-width");
+            expect(l.toPhysical("border-block-start-width", "vertical-rl", "rtl")).toBe("border-right-width");
+            expect(l.toPhysical("border-block-end-width", "vertical-rl", "rtl")).toBe("border-left-width");
+        });
+
+        it("converts logical properties to physical properties (vertical-lr, ltr)", function() {
             expect(l.toPhysical("inline-start", "vertical-lr")).toBe("top");
             expect(l.toPhysical("inline-end", "vertical-lr")).toBe("bottom");
             expect(l.toPhysical("block-start", "vertical-lr")).toBe("left");
@@ -35,6 +57,17 @@ describe("logical", function() {
             expect(l.toPhysical("border-inline-end-width", "vertical-lr")).toBe("border-bottom-width");
             expect(l.toPhysical("border-block-start-width", "vertical-lr")).toBe("border-left-width");
             expect(l.toPhysical("border-block-end-width", "vertical-lr")).toBe("border-right-width");
+        });
+
+        it("converts logical properties to physical properties (vertical-lr, rtl)", function() {
+            expect(l.toPhysical("inline-start", "vertical-lr", "rtl")).toBe("bottom");
+            expect(l.toPhysical("inline-end", "vertical-lr", "rtl")).toBe("top");
+            expect(l.toPhysical("block-start", "vertical-lr", "rtl")).toBe("left");
+            expect(l.toPhysical("block-end", "vertical-lr", "rtl")).toBe("right");
+            expect(l.toPhysical("border-inline-start-width", "vertical-lr", "rtl")).toBe("border-bottom-width");
+            expect(l.toPhysical("border-inline-end-width", "vertical-lr", "rtl")).toBe("border-top-width");
+            expect(l.toPhysical("border-block-start-width", "vertical-lr", "rtl")).toBe("border-left-width");
+            expect(l.toPhysical("border-block-end-width", "vertical-lr", "rtl")).toBe("border-right-width");
         });
 
         it("returns original properties when they are already physical", function() {
@@ -46,7 +79,7 @@ describe("logical", function() {
     });
 
     describe("toLogical", function() {
-        it("converts physical properties to logical properties (horizontal-tb)", function() {
+        it("converts physical properties to logical properties (horizontal-tb, ltr)", function() {
             expect(l.toLogical("left", "horizontal-tb")).toBe("inline-start");
             expect(l.toLogical("right", "horizontal-tb")).toBe("inline-end");
             expect(l.toLogical("top", "horizontal-tb")).toBe("block-start");
@@ -57,7 +90,18 @@ describe("logical", function() {
             expect(l.toLogical("border-bottom-width", "horizontal-tb")).toBe("border-block-end-width");
         });
 
-        it("converts physical properties to logical properties (vertical-rl)", function() {
+        it("converts physical properties to logical properties (horizontal-tb, rtl)", function() {
+            expect(l.toLogical("left", "horizontal-tb", "rtl")).toBe("inline-end");
+            expect(l.toLogical("right", "horizontal-tb", "rtl")).toBe("inline-start");
+            expect(l.toLogical("top", "horizontal-tb", "rtl")).toBe("block-start");
+            expect(l.toLogical("bottom", "horizontal-tb", "rtl")).toBe("block-end");
+            expect(l.toLogical("border-left-width", "horizontal-tb", "rtl")).toBe("border-inline-end-width");
+            expect(l.toLogical("border-right-width", "horizontal-tb", "rtl")).toBe("border-inline-start-width");
+            expect(l.toLogical("border-top-width", "horizontal-tb", "rtl")).toBe("border-block-start-width");
+            expect(l.toLogical("border-bottom-width", "horizontal-tb", "rtl")).toBe("border-block-end-width");
+        });
+
+        it("converts physical properties to logical properties (vertical-rl, ltr)", function() {
             expect(l.toLogical("left", "vertical-rl")).toBe("block-end");
             expect(l.toLogical("right", "vertical-rl")).toBe("block-start");
             expect(l.toLogical("top", "vertical-rl")).toBe("inline-start");
@@ -68,7 +112,18 @@ describe("logical", function() {
             expect(l.toLogical("border-bottom-width", "vertical-rl")).toBe("border-inline-end-width");
         });
 
-        it("converts logical properties to physical properties (vertical-lr)", function() {
+        it("converts physical properties to logical properties (vertical-rl, rtl)", function() {
+            expect(l.toLogical("left", "vertical-rl", "rtl")).toBe("block-end");
+            expect(l.toLogical("right", "vertical-rl", "rtl")).toBe("block-start");
+            expect(l.toLogical("top", "vertical-rl", "rtl")).toBe("inline-end");
+            expect(l.toLogical("bottom", "vertical-rl", "rtl")).toBe("inline-start");
+            expect(l.toLogical("border-left-width", "vertical-rl", "rtl")).toBe("border-block-end-width");
+            expect(l.toLogical("border-right-width", "vertical-rl", "rtl")).toBe("border-block-start-width");
+            expect(l.toLogical("border-top-width", "vertical-rl", "rtl")).toBe("border-inline-end-width");
+            expect(l.toLogical("border-bottom-width", "vertical-rl", "rtl")).toBe("border-inline-start-width");
+        });
+
+        it("converts logical properties to physical properties (vertical-lr, ltr)", function() {
             expect(l.toLogical("left", "vertical-lr")).toBe("block-start");
             expect(l.toLogical("right", "vertical-lr")).toBe("block-end");
             expect(l.toLogical("top", "vertical-lr")).toBe("inline-start");
@@ -77,6 +132,17 @@ describe("logical", function() {
             expect(l.toLogical("border-right-width", "vertical-lr")).toBe("border-block-end-width");
             expect(l.toLogical("border-top-width", "vertical-lr")).toBe("border-inline-start-width");
             expect(l.toLogical("border-bottom-width", "vertical-lr")).toBe("border-inline-end-width");
+        });
+
+        it("converts logical properties to physical properties (vertical-lr, rtl)", function() {
+            expect(l.toLogical("left", "vertical-lr", "rtl")).toBe("block-start");
+            expect(l.toLogical("right", "vertical-lr", "rtl")).toBe("block-end");
+            expect(l.toLogical("top", "vertical-lr", "rtl")).toBe("inline-end");
+            expect(l.toLogical("bottom", "vertical-lr", "rtl")).toBe("inline-start");
+            expect(l.toLogical("border-left-width", "vertical-lr", "rtl")).toBe("border-block-start-width");
+            expect(l.toLogical("border-right-width", "vertical-lr", "rtl")).toBe("border-block-end-width");
+            expect(l.toLogical("border-top-width", "vertical-lr", "rtl")).toBe("border-inline-end-width");
+            expect(l.toLogical("border-bottom-width", "vertical-lr", "rtl")).toBe("border-inline-start-width");
         });
 
         it("returns original properties when they are already logical", function() {
