7e34ef1249dbc4a469be7c3b887e189531a8da74
KAWAKUBO Toru <kwkbtr@vivliostyle.com>
2016-01-15 13:19:49 +0900

    Include 'Supported features' document in the distribution package and gh-pages

--- scripts/deploy-viewer-to-gh-pages.sh
+++ scripts/deploy-viewer-to-gh-pages.sh
@@ -6,6 +6,11 @@ echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy
 echo -e "$GITHUB_DEPLOY_KEY" | base64 -d > ~/.ssh/deploy.key
 chmod 600 ~/.ssh/deploy.key
 
+# build documents
+gem install kramdown
+doc_dir="node_modules/vivliostyle/doc"
+kramdown --no-auto-ids -i GFM --template ${doc_dir}/supported-features.erb ${doc_dir}/supported-features.md > ${doc_dir}/supported-features.html
+
 cd ../
 
 # fetch gh-pages branch
@@ -18,6 +23,7 @@ git config user.name "kwkbtr (Travis CI)"
 
 # update gh-pages branch
 cp -R ../vivliostyle-ui/node_modules/vivliostyle/samples/* samples/
+cp ../vivliostyle-ui/node_modules/vivliostyle/doc/supported-features.{md,html} docs/
 cp -R ../vivliostyle-ui/build/* viewer/
 
 zip="../vivliostyle-ui/vivliostyle-js-latest.zip"
