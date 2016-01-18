3b8f76e85167d2e8c0cce4c7cebd71d2cdad01dc
KAWAKUBO Toru <kwkbtr@vivliostyle.com>
2016-01-18 14:20:40 +0900

    Include release version number in 'Supported Features' document - Only include the version if it is a release version.

--- build/travis-after-success.sh
+++ build/travis-after-success.sh
@@ -4,7 +4,15 @@ set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" -a \( "${TRAVIS_BRANCH}" = "master" -o "${TRAVIS_TAG}" != "" \) ]; then
     # build documents
     gem install kramdown
-    kramdown --no-auto-ids -i GFM --template doc/supported-features.erb doc/supported-features.md > doc/supported-features.html
+    src="doc/supported-features.md"
+    template=doc/supported-features.erb
+    version=$(grep '^ *"version":' package.json | sed -e 's/^.*"\([^"]*\)",$/\1/')
+    if [ $(echo ${version} | grep -- '-pre') ]; then
+        sed -i -e "s/<%= \$version %>//" ${src} ${template}
+    else
+        sed -i -e "s/<%= \$version %>/ ${version}/" ${src} ${template}
+    fi
+    kramdown --no-auto-ids -i GFM --template ${template} ${src} > doc/supported-features.html
 
     build/npm-publish.sh
     build/deploy-viewer-to-gh-pages.sh
