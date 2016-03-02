23b1198037db8ee2f68441ca324e151943a03f17
KAWAKUBO Toru <kwkbtr@vivliostyle.com>
2016-03-02 22:25:33 +0900

    Fix flex container pagination problem: make flex containers unbreakable
    Flex containers are treated as unbreakable boxes for now.

--- src/adapt/layout.js
+++ src/adapt/layout.js
@@ -1567,7 +1567,7 @@ adapt.layout.Column.prototype.isBreakable = function(flowPosition) {
 	case adapt.base.NS.SVG:
 		return false;
 	}
-	return true;
+	return !flowPosition.flexContainer;
 };
 
 /**
@@ -1721,8 +1721,8 @@ adapt.layout.Column.prototype.skipEdges = function(nodeContext, leadingEdge) {
 						// clear
 						self.applyClearance(nodeContext);
 					}
-					if (nodeContext.floatSide) {
-						// float
+					if (nodeContext.floatSide || nodeContext.flexContainer) {
+						// float or flex container (unbreakable)
 						if (self.saveEdgeAndCheckForOverflow(lastAfterNodeContext, null, true, breakAtTheEdge)) {
 							// overflow
 					    	nodeContext = (lastAfterNodeContext || nodeContext).modify();
