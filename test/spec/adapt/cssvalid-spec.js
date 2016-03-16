95932d5ba35fed49a625c7ae68c6bbf6cd9703a0
Satoshi KOJIMA <skoji@mac.com>
2016-03-16 22:08:52 +0900

    add COMMA and SPACE simple test

--- test/spec/adapt/cssvalid-spec.js
+++ test/spec/adapt/cssvalid-spec.js
@@ -18,5 +18,42 @@ describe("cssvalid", function() {
           });
       });
     });
+    it("sould parse validator and space rule", function(done) {
+      var validatorSet = new adapt.cssvalid.ValidatorSet();
+      validatorSet.initBuiltInValidators();
+      validatorSet.parse("foo = SPACE(IDENT+);");
+      var handler = new adapt.csscasc.CascadeParserHandler(null, null, null, null, null,
+	    		                                           validatorSet, true);
+      var warnListener = jasmine.createSpy("warn listener");
+      vivliostyle.logging.logger.addListener(vivliostyle.logging.LogLevel.WARN, warnListener);
+      adapt.task.start(function() {
+        adapt.cssparse.parseStylesheetFromText(
+          ".test { foo: bar; }\n.test2 { foo: bar baz boo; }", handler, null, null, null).then(
+          function(result) {
+            expect(result).toBe(true);
+            expect(warnListener).not.toHaveBeenCalled();
+            done();
+          });
+      });
+    });
+    it("sould parse validator and comma rule", function(done) {
+      var validatorSet = new adapt.cssvalid.ValidatorSet();
+      validatorSet.initBuiltInValidators();
+      validatorSet.parse("foo = COMMA( IDENT+ );");
+      var handler = new adapt.csscasc.CascadeParserHandler(null, null, null, null, null,
+	    		                                           validatorSet, true);
+      var warnListener = jasmine.createSpy("warn listener");
+      vivliostyle.logging.logger.addListener(vivliostyle.logging.LogLevel.WARN, warnListener);
+      adapt.task.start(function() {
+        adapt.cssparse.parseStylesheetFromText(
+          ".test { foo: bar,baz; }\n .test2{ foo: bar; }", handler, null, null, null).then(
+          function(result) {
+            expect(result).toBe(true);
+            expect(warnListener).not.toHaveBeenCalled();
+            done();
+          });
+      });
+    });
+    
   });
 });
