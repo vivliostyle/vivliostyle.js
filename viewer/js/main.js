b7e780c98061b0a5f74304b79897cb7bd38f21ad
kwkbtr (Travis CI) <kwkbtr@vivliostyle.com>
2016-04-15 05:37:35 +0000

    Update vivliostyle.js (original commit: 7e1728a133225cce4b5acdf6ace303c8114d9061)

--- viewer/js/main.js
+++ viewer/js/main.js
@@ -5888,7 +5888,7 @@ ko.exportSymbol('nativeTemplateEngine', ko.nativeTemplateEngine);
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Vivliostyle core 2016.4.1-pre.20160412013119
+ * Vivliostyle core 2016.4.1-pre.20160415053057
  */
 (function(factory) {
     if (typeof define === "function" && define.amd) {
