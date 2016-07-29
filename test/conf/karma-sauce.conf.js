6fd21874ae32929b10814b4e6454e1b46e01d4fe
KAWAKUBO Toru <kwkbtr@vivliostyle.com>
2016-07-29 12:05:03 +0900

    Fix test configuration: remove Edge (fails to start on SauceLabs)

--- test/conf/karma-sauce.conf.js
+++ test/conf/karma-sauce.conf.js
@@ -35,11 +35,6 @@ module.exports = function(config) {
             browserName: "safari",
             platform: "OS X 10.11"
         },
-        sl_edge_11: {
-            base: "SauceLabs",
-            browserName: "edge",
-            platform: "Windows 10"
-        },
         sl_ie_11: {
             base: "SauceLabs",
             browserName: "internet explorer",
