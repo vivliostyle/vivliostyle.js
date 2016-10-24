68b942c90073f01b58c2e55c8962613991dcda3b
unageanu <masaya.yamauchi@gmail.com>
2016-10-24 14:59:02 +0900

    Support Common JS modules.

--- src/vivliostyle.js
+++ src/vivliostyle.js
@@ -5,14 +5,16 @@
     "use strict";
 
     function getRootPathAndScriptTag(rootScriptName) {
-        var pattern = new RegExp("/" + rootScriptName + "$");
+        var pattern = new RegExp("/src/" + rootScriptName + "$");
         var scriptTags = document.getElementsByTagName("script");
         for (var i = 0; i < scriptTags.length; i++) {
             var tag = scriptTags[i];
             var src = tag.src;
             if (src.match(pattern)) {
+                var base = src.replace(pattern, "/");
                 return {
-                    path: src.replace(pattern, "/"),
+                    path: base +  "src/",
+                    commonJsModuleBasePath: base,
                     tag: tag
                 };
             }
@@ -20,13 +22,31 @@
         throw new Error("Cannot find a script tag for " + rootScriptName);
     }
 
+    // An emulation of nodejs require (only for internal modules).
+    window.module = {};
+    window.require = function(name) {
+        if (window.module[name]) {
+            return window.module[name];
+        }
+        throw new Error("module '" + name + "' not found!");
+    };
+
     var pathAndTag = getRootPathAndScriptTag("vivliostyle.js");
 
     function loadScript(relativePath) {
         document.write("<script src='" + pathAndTag.path + relativePath + "' type='text/javascript'></script>");
     }
+    function loadCommonJsModule(relativePath) {
+        document.write("<script src='" + pathAndTag.commonJsModuleBasePath
+            + relativePath + "' type='text/javascript'></script>"
+            + "<script type='text/javascript'>"
+            + "var moduleName = \"" + relativePath.replace(/\.[a-zA-Z0-9]*$/, '') + "\";"
+            + "module[moduleName] = module.exports;"
+            + "</script>");
+    }
 
-    window.vivliostyleCallback = function(scriptPaths) {
+    window.vivliostyleCallback = function(scriptPaths, commonJsModulePaths) {
+        commonJsModulePaths.forEach(loadCommonJsModule);
         scriptPaths.forEach(loadScript);
     };
     loadScript("source-list.js");
