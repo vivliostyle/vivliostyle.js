f6bc2bc0762ec72d36adbc6350c7c6271e2d4c45
unageanu <masaya.yamauchi@gmail.com>
2017-01-17 15:25:56 +0900

    Suppress ESLint warnings

--- src/vivliostyle/table.js
+++ src/vivliostyle/table.js
@@ -1378,7 +1378,7 @@ goog.scope(function() {
             this, column, nodeContext, forceRemoveSelf, endOfRegion).then(function(result) {
                 vivliostyle.repetitiveelements.appendFooterToAncestors(nodeContext);
                 frame.finish(result);
-            }.bind(this));
+            });
         return frame.result();
     };
 
@@ -1541,7 +1541,7 @@ goog.scope(function() {
     TableRowLayoutConstraint.prototype.nextCandidate = function(nodeContext) {
         var formattingContext = getTableFormattingContext(this.nodeContext.formattingContext);
         var repetitiveElements = this.getRepetitiveElements();
-        if (!repetitiveElements) return;
+        if (!repetitiveElements) return false;
         if (formattingContext.isAfterContextOfRootElement(nodeContext)
             && repetitiveElements.isSkipFooter) {
             repetitiveElements.preventSkippingFooter();
