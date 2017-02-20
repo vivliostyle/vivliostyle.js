a362281be0c0c052e7d9356bf7a8acc18660ecdc
kwkbtr (Travis CI) <kwkbtr@vivliostyle.com>
2017-02-20 08:33:14 +0000

    Update vivliostyle.js (original commit: 5a347d11eb9177bb93a6e27fcd99c12f084994f3)

--- viewer/js/main.js
+++ viewer/js/main.js
@@ -5899,19 +5899,20 @@ ko.exportSymbol('nativeTemplateEngine', ko.nativeTemplateEngine);
  * Copyright 2013 Google, Inc.
  * Copyright 2015 Vivliostyle Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Vivliostyle.js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Vivliostyle.js is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Vivliostyle.js.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Vivliostyle core 2016.10.1-pre.20170217014745
+ * Vivliostyle core 2016.10.1-pre.20170220082524
  */
 (function(factory) {
     if (typeof define === "function" && define.amd) {
@@ -6639,6 +6640,7 @@ a.userStyleSheet||null,spreadView:l,pageBorder:1};var m;if(e&&f)m=e+" "+f;else{s
 g.textContent="@page { size: "+m+"; margin: 0; }",document.head.appendChild(g));ft(new Fs(window,k,"main",Hu),a)});
     return enclosingObject.vivliostyle;
 }.bind(window));
+
 
 },{}],3:[function(require,module,exports){
 /*
