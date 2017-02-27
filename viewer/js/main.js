81af1742ab8ef17e7e75622d8d321aecfbf9a7f4
kwkbtr (Travis CI) <kwkbtr@vivliostyle.com>
2017-02-27 05:24:33 +0000

    Update vivliostyle.js (original commit: 4611a355b0fd68270dfddecf66941a5341314a62)

--- viewer/js/main.js
+++ viewer/js/main.js
@@ -5912,7 +5912,7 @@ ko.exportSymbol('nativeTemplateEngine', ko.nativeTemplateEngine);
  * You should have received a copy of the GNU Affero General Public License
  * along with Vivliostyle.js.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Vivliostyle core 2017.2
+ * Vivliostyle core 2017.2.1-pre.20170227051712
  */
 (function(factory) {
     if (typeof define === "function" && define.amd) {
