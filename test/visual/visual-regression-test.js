af3a6353d55c6dbba335e88c9dc3a40fad745966
KAWAKUBO Toru <kwkbtr@vivliostyle.com>
2017-06-20 11:57:17 +0900

    Introduce visual regression test using WebdriverIO and wdio-visual-regression-service
    TODO:
    - Add visual regression test launcher using Vivliostyle
    - Add a task for generating reference screenshots

---
+++ test/visual/visual-regression-test.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2017 Vivliostyle Inc.
+ *
+ * Vivliostyle.js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vivliostyle.js is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Vivliostyle.js.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+describe("Visual regression tests", () => {
+    it("diff", () => {
+        browser.url("files/");
+        browser.pause(500);
+        const report = browser.checkDocument();
+        report.forEach((result) => expect(result.isExactSameImage).toBe(true));
+    });
+});
