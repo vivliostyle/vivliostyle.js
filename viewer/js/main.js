9428b252db1a647ba0f44a1f52bebacfdde5710a
MurakamiShinyu (Travis CI) <murakami@vivliostyle.org>
2019-04-01 17:14:07 +0000

    Update vivliostyle.js (original commit: c8bf32c0eaa459fe7d9f8e35cf0e5585938219d2)

--- viewer/js/main.js
+++ viewer/js/main.js
@@ -3698,7 +3698,7 @@ module.exports = exports["default"];
  * You should have received a copy of the GNU Affero General Public License
  * along with Vivliostyle.js.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Vivliostyle core 2019.1.103-pre.20190401161635
+ * Vivliostyle core 2019.1.103-pre.20190401171152
  */
 (function(factory) {
     if (typeof define === "function" && define.amd) {
