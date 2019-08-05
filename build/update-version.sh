f9cd512ab9154fad3b2ce3aa6b696c4827430077
MurakamiShinyu <murakami@vivliostyle.org>
2019-08-05 22:42:57 +0900

    Update build/update-version.sh: 'src/wrapper.js' no longer exists

--- build/update-version.sh
+++ build/update-version.sh
@@ -6,6 +6,3 @@ if [ $(echo ${version} | grep -- '-pre$') ]; then
     version=${version}.$(date -u "+%Y%m%d%H%M%S")
     npm --no-git-tag-version version ${version}
 fi
-
-version=$(echo ${version} | sed -e 's/\.0$//')
-sed -i -e "s/%version%/${version}/" src/wrapper.js
