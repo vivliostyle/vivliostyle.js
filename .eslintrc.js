2363b1ad252a7b6b4808fb1ec3c8593a1438598f
Shota Kubota <ta2@o2p.jp>
2019-09-19 10:35:13 +0900

    add ESLint config file

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings"
+    ],
+    "plugins": [
+        "import",
+        "prettier"
+    ],
+    "rules": {
+    }
+};
