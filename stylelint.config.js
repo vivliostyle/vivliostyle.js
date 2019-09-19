884f035ecda6a2f3a7b95b697d2c629b70cbf15a
Shota Kubota <ta2@o2p.jp>
2019-09-19 20:28:04 +0900

    add Stylelint config file

---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: ["stylelint-scss"],
+    extends: "stylelint-config-recommended",
+    rules: {
+        "at-rule-no-unknown": [true, { ignoreAtRules: ["include", "mixin", "if", "else"] }]
+    }
+};
