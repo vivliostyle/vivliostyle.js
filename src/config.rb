aa95cf88471ef715092fefcd6cd808cf0c1aa519
Shota Kubota <ta2@o2p.jp>
2019-09-20 15:20:03 +0900

    remove files related to compass

--- src/config.rb
+++
@@ -1,7 +0,0 @@
-relative_assets = true
-project_path = "../"
-sass_dir = "src/scss"
-css_dir = "build/css"
-fonts_dir = "build/fonts"
-output_style = ( environment == :production ) ? :compressed : :expanded
-line_comments = ( environment == :production ) ? false : true
