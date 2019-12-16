2379af8f94bd975e2100d6a94559502df7010e33
Yasuaki Uechi <y@uechi.io>
2019-12-16 17:42:53 +0900

    chore(ci): fix deploy keys

--- scripts/after-success-hook.sh
+++ scripts/after-success-hook.sh
@@ -7,13 +7,9 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; 
     ARCHIVE_PATH="vivliostyle.zip"
     VIEWER_ARTIFACTS=packages/viewer/lib/*
 
-    # config
+    # git
     git config user.email "murakami@vivliostyle.org"
     git config user.name "MurakamiShinyu (CI)"
-    echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config
-    echo -e "$GITHUB_DEPLOY_KEY" | base64 -d > ~/.ssh/deploy.key
-    chmod 600 ~/.ssh/deploy.key
-    echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" > ~/.npmrc
 
     # generate archive
     scripts/generate-viewer-archive.sh "${ARCHIVE_DIR}" "${ARCHIVE_PATH}"
@@ -21,7 +17,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; 
     # fetch vivliostyle.github.io
     git clone --depth=1 --branch=master git@github.com:vivliostyle/vivliostyle.github.io.git vivliostyle.github.io
 
-    # copy canary viewer
+    # copy canary viewer to vivliostyle.github.io
     CANARY_VIEWER_ROOT="vivliostyle.github.io/viewer"
     mkdir -p "${CANARY_VIEWER_ROOT}"
     cp -R ${VIEWER_ARTIFACTS} "${CANARY_VIEWER_ROOT}/"
@@ -32,19 +28,13 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; 
         VERSION=$(echo ${TRAVIS_TAG} | sed 's/^v\(.*\)/\1/')
         TAGGED_VIEWER_ROOT="vivliostyle.github.io/viewer/${TRAVIS_TAG}"
 
-        # Publish to npm
-        # TODO:
-        # yarn ship --yes
-
-        # GitHub Release
-        # TODO: upload $ARCHIVE_PATH to github/release/$TRAVIS_TAG as "vivliostyle-${VERSION}.zip"
-
-        # vivliostyle.github.io
+        # copy tagged resources to vivliostyle.github.io
         mkdir -p "${TAGGED_VIEWER_ROOT}"
         cp -R ${VIEWER_ARTIFACTS} "${TAGGED_VIEWER_ROOT}/"
         # cp -R "${ARCHIVE_PATH}" "${TAGGED_VIEWER_ROOT}/vivliostyle-${VERSION}.zip"
+        echo "${TRAVIS_TAG},${VERSION}" >> vivliostyle.github.io/_data/releases.csv
 
-        # vivliostyle.org
+        # copy latest resources to vivliostyle.org
         git clone --depth=1 --branch=master git@github.com:vivliostyle/vivliostyle.org.git vivliostyle.org
         mkdir -p "vivliostyle.org/viewer"
         cp -R ${VIEWER_ARTIFACTS} "vivliostyle.org/viewer/"
@@ -53,15 +43,26 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; 
         cp -R docs/user-guide/* vivliostyle.org/docs/user-guide/
         cp    docs/supported-features.md vivliostyle.org/docs/
 
+        # commit changes to vivliostyle.org
+        echo -e "$GITHUB_DEPLOY_KEY_VIVLIOSTYLE_ORG" | base64 -d > ~/.ssh/deploy-vivliostyle-org.key
+        chmod 600 ~/.ssh/deploy-vivliostyle-org.key
+        echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy-vivliostyle-org.key\n" >> ~/.ssh/config
         cd vivliostyle.org
         git add .
         git status
         git commit -m "Update vivliostyle latest release (original commit: $TRAVIS_COMMIT)"
         git push origin master
         cd ..
+
+        # publish to npm
+        echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" > ~/.npmrc
+        yarn ship --yes
     fi
 
-    # commit changes
+    # commit changes to vivliostyle.github.io
+    echo -e "$GITHUB_DEPLOY_KEY_VIVLIOSTYLE_GITHUB_IO" | base64 -d > ~/.ssh/deploy-vivliostyle-github-io.key
+    chmod 600 ~/.ssh/deploy-vivliostyle-github-io.key
+    echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy-vivliostyle-github-io.key\n" >> ~/.ssh/config
     cd vivliostyle.github.io
     git add .
     git status
@@ -69,5 +70,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; 
     git push origin master
     cd ..
 
-    rm -rf vivliostyle.github.io vivliostyle.org vivliostyle.zip
+    # cleanup
+    rm -rf vivliostyle.github.io vivliostyle.org
+    echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config
 fi
