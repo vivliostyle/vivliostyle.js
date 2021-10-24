d22694cc678208cb385ce13704fcf4d002f4f14b
Satoshi Kojima <skoji@skoji.jp>
2021-10-24 10:55:30 +0900

    highlight by selection range

--- packages/viewer/src/bindings/textSelection.ts
+++ packages/viewer/src/bindings/textSelection.ts
@@ -21,16 +21,37 @@
 import ko from "knockout";
 
 const supportTouchEvents = "ontouchstart" in window;
+
+const highlightSelection = (selection: Selection, parent: HTMLElement) => {
+  if (selection.type == "Range") {
+    const range = selection.getRangeAt(0);
+    const rects = range.getClientRects();
+    for (const rect of rects) {
+      const div = document.createElement("div");
+      div.style.position = "absolute";
+      div.style.margin = "0";
+      div.style.padding = "0";
+      div.style.top = `${rect.top - parent.offsetTop}px`;
+      div.style.left = `${rect.left - parent.offsetLeft}px`;
+      div.style.width = `${rect.width}px`;
+      div.style.height = `${rect.height}px`;
+      div.style.background = "rgba(255, 0, 0, 0.2)";
+      parent.appendChild(div);
+    }
+    selection.empty();
+  }
+};
+
 ko.bindingHandlers.textSelection = {
   init(element, valueAccessor): void {
     if (ko.unwrap(valueAccessor())) {
       if (supportTouchEvents) {
         element.addEventListener("touchend", () => {
-          console.log(document.getSelection());
+          highlightSelection(document.getSelection(), element);
         });
       } else {
         element.addEventListener("mouseup", () => {
-          console.log(document.getSelection());
+          highlightSelection(document.getSelection(), element);
         });
       }
     }
