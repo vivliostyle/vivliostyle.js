b81023090161094b0c23c81e7142bd1bf139395c
MurakamiShinyu <murakami@vivliostyle.org>
2025-11-23 11:36:47 +0900

    Replace SauceLabs with GitHub Actions matrix testing
    - Add browser launchers (karma-chrome-launcher, karma-firefox-launcher, karma-safari-launcher, karma-webkit-launcher)
    - Create karma-ci.conf.js with environment-variable-based browser selection
    - Add test-ci npm script for CI environments
    - Implement GitHub Actions matrix strategy:
      - Test across ubuntu-latest, windows-latest, macos-latest
      - Test across Chrome, Firefox browsers
      - Safari testing on macOS only
    - Split CI workflow into separate build and test jobs
    - Set fail-fast: false to run all matrix combinations even if one fails

---
+++ packages/core/test/conf/karma-ci.conf.js
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2017 Daishinsha Inc.
+ * Copyright 2025 Vivliostyle Foundation
+ *
+ * Vivliostyle.js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vivliostyle.js is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Vivliostyle.js.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*eslint-env node */
+/*eslint global-require: "off" */
+
+module.exports = function (config) {
+  var commonConfig = require("./karma-common.conf")(config);
+
+  // Get browser from environment variable or default to Chrome
+  var browser = process.env.TEST_BROWSER || "ChromeHeadless";
+
+  // Browser configuration
+  var browsers;
+  var customLaunchers = {};
+
+  if (browser === "Safari") {
+    // Safari doesn't have a headless mode
+    browsers = ["Safari"];
+  } else if (browser === "Firefox") {
+    browsers = ["FirefoxHeadless"];
+    // Set MOZ_HEADLESS environment variable for Firefox headless mode
+    process.env.MOZ_HEADLESS = "1";
+    customLaunchers.FirefoxHeadless = {
+      base: "Firefox",
+      displayName: "FirefoxHeadless",
+    };
+  } else {
+    // Default to Chrome
+    browsers = ["ChromeHeadless"];
+    customLaunchers.ChromeHeadless = {
+      base: "Chrome",
+      flags: [
+        "--headless",
+        "--disable-gpu",
+        "--no-sandbox",
+        "--disable-dev-shm-usage",
+      ],
+    };
+  }
+
+  var options = {
+    reporters: ["verbose"],
+    browsers: browsers,
+    customLaunchers: customLaunchers,
+    singleRun: true,
+    captureTimeout: 120000,
+    browserNoActivityTimeout: 120000,
+  };
+
+  for (var key in commonConfig) {
+    if (commonConfig.hasOwnProperty(key)) {
+      options[key] = commonConfig[key];
+    }
+  }
+
+  config.set(options);
+};
