1d8d0f3011f2dfd3f5e65d10d111e1c383850b8f
MurakamiShinyu <murakami@vivliostyle.org>
2026-03-30 12:32:11 +0900

    fix: prevent duplicate dev server startup

--- packages/viewer/gulpfile.js
+++ packages/viewer/gulpfile.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require("fs");
 const path = require("path");
+const process = require("process");
 // const KarmaServer = require("karma").Server;
 const browserSync = require("browser-sync").create();
 const gulp = require("gulp");
@@ -33,6 +34,8 @@ const VIEWER_JS_FILES = {
   production: path.relative(DEST_DIR, pkg.main),
   development: "js/vivliostyle-viewer-dev.js",
 };
+const DEV_SERVER_LOCK_FILE = path.resolve(".cache", "dev-server.json");
+const DEV_SERVER_DEFAULT_URL = "http://localhost:3000/core/test/files/";
 
 const VIVLIOSTYLE_VERSION = process.env["VIVLIOSTYLE_VERSION"];
 const VERSION =
@@ -48,6 +51,119 @@ function destDir(type) {
   const dirs = RESOURCE_MAP[type];
   const dirName = dirs.dest ? dirs.dest : dirs.src;
   return path.join(DEST_DIR, dirName);
+}
+
+function isProcessAlive(pid) {
+  if (!Number.isInteger(pid) || pid <= 0) {
+    return false;
+  }
+  try {
+    process.kill(pid, 0);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+function readDevServerLock() {
+  if (!fs.existsSync(DEV_SERVER_LOCK_FILE)) {
+    return null;
+  }
+
+  try {
+    return JSON.parse(fs.readFileSync(DEV_SERVER_LOCK_FILE, "utf8"));
+  } catch {
+    return null;
+  }
+}
+
+function removeDevServerLock() {
+  fs.rmSync(DEV_SERVER_LOCK_FILE, { force: true });
+}
+
+function cleanupStaleDevServerLock() {
+  const lock = readDevServerLock();
+  if (lock && isProcessAlive(lock.pid)) {
+    return lock;
+  }
+  if (fs.existsSync(DEV_SERVER_LOCK_FILE)) {
+    removeDevServerLock();
+  }
+  return null;
+}
+
+function writeDevServerLock(url) {
+  fs.mkdirSync(path.dirname(DEV_SERVER_LOCK_FILE), { recursive: true });
+  fs.writeFileSync(
+    DEV_SERVER_LOCK_FILE,
+    JSON.stringify({
+      pid: process.pid,
+      url,
+      startedAt: new Date().toISOString(),
+    }),
+  );
+}
+
+let hasRegisteredDevServerCleanup = false;
+function registerDevServerCleanup() {
+  if (hasRegisteredDevServerCleanup) {
+    return;
+  }
+  hasRegisteredDevServerCleanup = true;
+
+  const cleanup = () => {
+    const lock = readDevServerLock();
+    if (lock && lock.pid === process.pid) {
+      removeDevServerLock();
+    }
+  };
+
+  process.on("exit", cleanup);
+  process.on("SIGINT", () => {
+    cleanup();
+    process.exit(130);
+  });
+  process.on("SIGTERM", () => {
+    cleanup();
+    process.exit(143);
+  });
+}
+
+function findRunningDevServer() {
+  const lock = cleanupStaleDevServerLock();
+  if (!lock) {
+    return null;
+  }
+  return {
+    url: lock.url || DEV_SERVER_DEFAULT_URL,
+  };
+}
+
+function getLocalServerOrigin(bs) {
+  if (!bs || typeof bs.getOption !== "function") {
+    return "http://localhost:3000";
+  }
+  const urls = bs.getOption("urls");
+  if (urls && typeof urls.get === "function") {
+    const local = urls.get("local");
+    if (local) {
+      return local;
+    }
+  }
+  return "http://localhost:3000";
+}
+
+function getDevServerUrl(localUrl) {
+  if (typeof localUrl !== "string" || !localUrl) {
+    return DEV_SERVER_DEFAULT_URL;
+  }
+
+  if (localUrl.includes("/core/test/files/")) {
+    return localUrl;
+  }
+
+  const normalized = localUrl.endsWith("/") ? localUrl.slice(0, -1) : localUrl;
+  return `${normalized}/core/test/files/`;
 }
 
 const srcPattern = (type) =>
@@ -193,16 +309,35 @@ gulp.task(
 
 // serve
 function serve(isDevelopment) {
-  browserSync.init({
-    server: {
-      baseDir: "../",
+  const runningDevServer = findRunningDevServer();
+  if (runningDevServer) {
+    console.log(
+      `[dev] Vivliostyle dev server is already running at ${runningDevServer.url}.`,
+    );
+    console.log("[dev] Skipping a new dev server start.");
+    return false;
+  }
+
+  browserSync.init(
+    {
+      server: {
+        baseDir: "../",
+      },
+      startPath: "/core/test/files/",
+      ghostMode: false, // do not mirror clicks, scrolls etc. between multiple browsers
+      notify: false, // do not show any notifications in the browser
+      port: 3000,
     },
-    startPath: "/core/test/files/",
-    ghostMode: false, // do not mirror clicks, scrolls etc. between multiple browsers
-    notify: false, // do not show any notifications in the browser
-  });
+    (_, bs) => {
+      const localOrigin = getLocalServerOrigin(bs);
+      const devServerUrl = getDevServerUrl(localOrigin);
+      writeDevServerLock(devServerUrl);
+      registerDevServerCleanup();
+    },
+  );
   const target = [DEST_DIR + "/**/*"];
   gulp.watch(target).on("change", browserSync.reload);
+  return true;
 }
 gulp.task(
   "serve",
@@ -211,12 +346,21 @@ gulp.task(
     done();
   }),
 );
-gulp.task(
-  "serve-dev",
-  gulp.series("watch-dev", function (done) {
-    serve(true);
+gulp.task("serve-dev", function (done) {
+  const runningDevServer = findRunningDevServer();
+  if (runningDevServer) {
+    console.log(
+      `[dev] Vivliostyle dev server is already running at ${runningDevServer.url}.`,
+    );
+    console.log("[dev] Skipping a new dev server start.");
     done();
-  }),
-);
+    return;
+  }
+
+  return gulp.series("watch-dev", function (next) {
+    serve(true);
+    next();
+  })(done);
+});
 
 gulp.task("default", gulp.task("serve-dev"));
